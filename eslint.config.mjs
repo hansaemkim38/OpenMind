4998379601fe4c73e82fde295060d794752cfdb5
bk-git-hub <bkprivate99@gmail.com>
2024-04-12 11:39:06 +0900

    eslint 설정 커밋

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,11 @@
 import globals from "globals";
+
+import path from "path";
+import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 import pluginJs from "@eslint/js";
-import { fileURLToPath } from "url";
-import path from "path";
 
-// CommonJS 변수를 모방합니다 -- CommonJS를 사용하지 않을 경우 필요하지 않습니다.
+// mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
@@ -12,9 +13,4 @@ const compat = new FlatCompat({
   recommendedConfig: pluginJs.configs.recommended,
 });
 
-const eslintConfig = [
-  { languageOptions: { globals: globals.browser } },
-  ...compat.extends("airbnb"),
-];
-
-export default eslintConfig;
+export default [{ languageOptions: { globals: globals.browser } }, ...compat.extends("airbnb")];
